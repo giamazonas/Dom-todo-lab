22d4d258314c305383b035c57cdb8b9fa26b77ec
giamazonas <skibinskirenee@gmail.com>
2022-02-03 21:47:33 -0500

    add css

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-console.log('sanity check')
+console.log('sanity check Thursday')
+//**    constants   */
+//__________________________________________\
+
+//*** VARIABLES   
+//___________________________________________
+
+
+//**      CACHED ELEMENTS  */
+//____________________________________________
 
 const input = document.getElementById('all-lista') 
-
 const btn = document.getElementById('submit-button')
-
 const ul  = document.getElementById('todo-list')
+const resetBtn = document.getElementById('reset')
+
+
+//** EVENT LISTENERS  */
+//____________________________________________
 
 btn.addEventListener('click', function(evt){
   const newLi = document.createElement('li')
@@ -13,16 +25,33 @@ btn.addEventListener('click', function(evt){
     document.querySelector('ul').appendChild(newLi)
     input.value = ''
   }
-})
-
-const resetBtn = document.getElementById('reset')
-
-btn.addEventListener('click', function(evt){
-
-})
-
-function logReset(resetBtn){
-  reset.textContent = `It's a new day! Time to make a new list.`}
+  }
+)
 
 
-console.log()
+    //**   FUNCTIONS    */
+
+
+
+
+
+
+
+//________________________________________
+// ** I AM STILL WORKING ON THIS **
+
+// add event listener to existing cached element reference
+//manipulate the DOM to remove individual items to do when clicked on 
+
+
+//______________________
+//reset
+
+// resetBtn.addEventListener('click', function(){
+//     // function logReset(resetBtn){ 
+//     return 
+// })
+
+// ul.addEventListener('click', true){
+//   input.value = ''
+// }
